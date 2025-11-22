615e9dfbc691767e391104ab0237a8f42f6254a8
Jochen Kupperschmidt <homework@nwsnet.de>
2025-11-22 03:25:54 +0100

    Introduce module-internal type alias `SendEventResult`

--- src/events.rs
+++ src/events.rs
@@ -21,24 +21,26 @@ pub(crate) struct EventSender {
     sender: Sender<Event>,
 }
 
+type SendEventResult = Result<(), SendError<Event>>;
+
 impl EventSender {
     fn new(sender: Sender<Event>) -> Self {
         Self { sender }
     }
 
-    pub(crate) fn send_tag_read(&self, tag: Tag) -> Result<(), SendError<Event>> {
+    pub(crate) fn send_tag_read(&self, tag: Tag) -> SendEventResult {
         self.send(Event::TagRead { tag })
     }
 
-    pub(crate) fn send_button_pressed(&self, button: Button) -> Result<(), SendError<Event>> {
+    pub(crate) fn send_button_pressed(&self, button: Button) -> SendEventResult {
         self.send(Event::ButtonPressed { button })
     }
 
-    pub(crate) fn send_shutdown_requested(&self) -> Result<(), SendError<Event>> {
+    pub(crate) fn send_shutdown_requested(&self) -> SendEventResult {
         self.send(Event::ShutdownRequested)
     }
 
-    fn send(&self, msg: Event) -> Result<(), SendError<Event>> {
+    fn send(&self, msg: Event) -> SendEventResult {
         self.sender.send(msg)
     }
 }
